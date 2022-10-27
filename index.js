41006104d161923fbd7408d46e6e3c25548e4c27
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-10-27 16:14:21 -0500

    [ADD] INDEX JS v2

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const menuItems = document.querySelectorAll(".menu-item");
 //messages starts
 const messagesNotification = document.querySelector("#messages-notifications");
 const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".messages");
+const messageSearch = document.querySelector("#message-search");
 
+
+//searchbar starts
+messageSearch.addEventListener("keyup", () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(item => {
+        let name = item.querySelectorAll("h5").textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            item.style.display = "flex";
+        } else {
+            item.style.display = "none";
+        }
+
+    });
+});
+//searchbar ends
+//highlight message menu item
 messagesNotification.addEventListener("click", () => {
     messages.style.boxShadow = "0 0 1rem var(--color-primary)";
     messagesNotification.querySelector('.notification-count').style.display = "none";
@@ -28,14 +46,14 @@ menuItems.forEach((menuItem) => {
     menuItem.addEventListener("click", () => {
         changeActiveItem();
         menuItem.classList.add("active");
-        if (menuItem.id != 'notifications'){
+        if (menuItem.id != 'notifications') {
             document.querySelector('.notifications-popup').
-            style.display = 'none';
+                style.display = 'none';
         } else {
             document.querySelector('.notifications-popup').
-            style.display = 'block';
+                style.display = 'block';
             document.querySelector('#notifications .notification-count').
-            style.display = 'none';
+                style.display = 'none';
         }
     });
 });
