3a3eb1a3ecfb5f9ba19f2767863bb15366e9a308
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-10-29 09:52:28 -0500

    [ADD] modal logic and fontsize logic v2

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const theme = document.querySelector("#theme");
 const themeModal = document.querySelector(".customize-theme");
 const fontSizes = document.querySelectorAll(".choose-size span");
 var root = document.querySelector(":root");
+const colorPalette = document.querySelectorAll(".choose-color span");
 
 //searchbar starts
 messageSearch.addEventListener("keyup", () => {
@@ -67,11 +68,11 @@ menuItems.forEach((menuItem) => {
 //theme customization starts
 //opens modal
 const openThemeModal = () => {
-  themeModal.style.display = "grid";
+    themeModal.style.display = "grid";
 };
 // closes modal
 const closeThemeModal = (e) => {
-    if(e.target.classList.contains("customize-theme")){
+    if (e.target.classList.contains("customize-theme")) {
         themeModal.style.display = "none";
     }
 };
@@ -82,44 +83,77 @@ theme.addEventListener("click", openThemeModal);
 //remove active class from spans
 
 const removeSizeSelector = () => {
-  fontSizes.forEach((item) => {
-    item.classList.remove("active");
-  });
+    fontSizes.forEach((item) => {
+        item.classList.remove("active");
+    });
 };
 
 
 
 //font size starts
-fontSizes.forEach(size => {  
+fontSizes.forEach(size => {
     size.addEventListener('click', () => {
-        removeSizeSelector();    
+        removeSizeSelector();
         let fontSize;
         size.classList.toggle('active')
 
-        if(size.classList.contains('font-size-1')){
+        if (size.classList.contains('font-size-1')) {
             fontSize = '10px';
             root.style.setProperty('----sticky-top-left', '5.4rem');
             root.style.setProperty('----sticky-top-right', '5.4rem');
-        } else if(size.classList.contains('font-size-2')){
+        } else if (size.classList.contains('font-size-2')) {
             fontSize = '13px';
             root.style.setProperty('----sticky-top-left', '5.4rem');
             root.style.setProperty('----sticky-top-right', '-7rem');
-        } else if(size.classList.contains('font-size-3')){
+        } else if (size.classList.contains('font-size-3')) {
             fontSize = '16px';
             root.style.setProperty('----sticky-top-left', '-2rem');
             root.style.setProperty('----sticky-top-right', '-17rem');
-        } else if(size.classList.contains('font-size-4')){
+        } else if (size.classList.contains('font-size-4')) {
             fontSize = '19px';
             root.style.setProperty('----sticky-top-left', '-5rem');
             root.style.setProperty('----sticky-top-right', '-25rem');
-        } else if(size.classList.contains('font-size-5')){
+        } else if (size.classList.contains('font-size-5')) {
             fontSize = '22px';
             root.style.setProperty('----sticky-top-left', '-12rem');
             root.style.setProperty('----sticky-top-right', '-35rem');
         }
 
-    //change font size
-    document.querySelector('html').style.fontSize = fontSize; 
+        //change font size
+        document.querySelector('html').style.fontSize = fontSize;
     });
 });
 //font size ends
+
+//remove active class from
+const changeActiveColorClass = () => {
+    colorPalette.forEach((colorPicker) => {
+        colorPicker.classList.remove("active");
+    });
+};
+
+//change primary colors starts
+
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        let primary;
+        changeActiveColorClass();
+        if (color.classList.contains("color-1")) {
+            primaryHue = 252;
+        } else if (color.classList.contains("color-2")) {
+            primaryHue = 52;
+        } else if (color.classList.contains("color-3")) {
+            primaryHue = 352;
+        } else if (color.classList.contains("color-4")) {
+            primaryHue = 152;
+        } else if (color.classList.contains("color-5")) {
+            primaryHue = 202;
+        }
+
+        color.classList.add("active");
+
+        root.style.setProperty('--primary-color-hue', primaryHue);
+
+    })
+});
+//change primary colors ends
