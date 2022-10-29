832f1f80b6f064ba4797194e180bc09dce7c5b2e
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-10-29 09:36:52 -0500

    [ADD] modal logic and fontsize logic

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const menuItems = document.querySelectorAll(".menu-item");
 //messages starts
 const messagesNotification = document.querySelector("#messages-notifications");
 const messages = document.querySelector(".messages");
-const message = messages.querySelectorAll(".messages");
+const message = messages.querySelectorAll(".message");
 const messageSearch = document.querySelector("#message-search");
 
+//theme
+const theme = document.querySelector("#theme");
+const themeModal = document.querySelector(".customize-theme");
+const fontSizes = document.querySelectorAll(".choose-size span");
+var root = document.querySelector(":root");
 
 //searchbar starts
 messageSearch.addEventListener("keyup", () => {
     const val = messageSearch.value.toLowerCase();
     message.forEach(item => {
-        let name = item.querySelectorAll("h5").textContent.toLowerCase();
+        let name = item.querySelectorAll('h5').textContent.toLowerCase();
         if (name.indexOf(val) != -1) {
             item.style.display = "flex";
         } else {
@@ -58,3 +63,63 @@ menuItems.forEach((menuItem) => {
     });
 });
 //sidebar ends
+
+//theme customization starts
+//opens modal
+const openThemeModal = () => {
+  themeModal.style.display = "grid";
+};
+// closes modal
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains("customize-theme")){
+        themeModal.style.display = "none";
+    }
+};
+themeModal.addEventListener("click", closeThemeModal);
+
+theme.addEventListener("click", openThemeModal);
+//theme customization ends
+//remove active class from spans
+
+const removeSizeSelector = () => {
+  fontSizes.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+
+
+
+//font size starts
+fontSizes.forEach(size => {  
+    size.addEventListener('click', () => {
+        removeSizeSelector();    
+        let fontSize;
+        size.classList.toggle('active')
+
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '5.4rem');
+        } else if(size.classList.contains('font-size-2')){
+            fontSize = '13px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '-7rem');
+        } else if(size.classList.contains('font-size-3')){
+            fontSize = '16px';
+            root.style.setProperty('----sticky-top-left', '-2rem');
+            root.style.setProperty('----sticky-top-right', '-17rem');
+        } else if(size.classList.contains('font-size-4')){
+            fontSize = '19px';
+            root.style.setProperty('----sticky-top-left', '-5rem');
+            root.style.setProperty('----sticky-top-right', '-25rem');
+        } else if(size.classList.contains('font-size-5')){
+            fontSize = '22px';
+            root.style.setProperty('----sticky-top-left', '-12rem');
+            root.style.setProperty('----sticky-top-right', '-35rem');
+        }
+
+    //change font size
+    document.querySelector('html').style.fontSize = fontSize; 
+    });
+});
+//font size ends
