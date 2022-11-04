d4cd44948cd9ab9546ec93880acdb9f064f84839
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-04 05:33:25 -0500

    ADD GO TOP BUTTON

--- index.js
+++ index.js
@@ -212,3 +212,22 @@ Bg3.addEventListener('click', () => {
 });
 
 //theme background values ends
+
+//go top starts
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container')
+            .classList.add('show');
+    } else {
+        document.querySelector(".go-top-container")
+            .classList.remove('show');
+    }
+};
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+});
+//go top ends
